cee10d287f7fedda8c799fee34a58d16aebe3c8d
gravity34031 <gravity2507@gmail.com>
2024-10-27 23:26:17 +0500

    added 6th spring support. Added copying spirin-dll from previous sprint when new solution is creating

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ class Program:
         # Поменяй projects_directory на папку, где лежат все спринты. Не конкретный открытый спринт 
         # !!! ОБЯЗАТЕЛЬНО В ПУТИ ДОЛЖНО БЫТЬ ДВА СЛЭША '\\' , НЕ ОДИН !!!
         # ЛИБО оставь projects_directory пустым и закинь файл в папку со всеми solution(спринтами)
-    projects_directory = "C:\\Users\\gravi\\Desktop\\projects\\asylum\\c# sprints\\"
+    projects_directory = "C:\\Users\\gravi\\Desktop\\projects\\asylum\\c# sprints"
         # Измени фамилию-имя-отчество на свое
     initials = "KomarovNA"
     initials_ru = "Комаров Никита Алексеевич"
         # !!! ИЗМЕНИ НОМЕР СПРИНТА !!!
-    sprint_number = 2
+    sprint_number = 5
         # change often
         # перед началом нового таска меняй ТЕМУ таска и ОПИСАНИЕ таска. Просто копируй с сайта
-    title = "Оператор if - полная и короткая форма записи"
+    title = ""
     condition = """
-    Написать программу на, которая запрашивает целые значения с клавиатуры и вычисляет находится ли точка с координатами X,Y в заштрихованной области."""
+    """
     ### END SETTINGS ###
     
     
@@ -41,12 +41,17 @@ class Program:
         self.solution_folder_name = f"Tyuiu.{self.initials}.Sprint{self.sprint_number}"
         self.path = os.path.join(self.projects_directory,self.solution_folder_name)
         self.solution_name = f"Tyuiu.{self.initials}.Sprint{self.sprint_number}.sln"
-                
+        
+        self.shit_dll = 'tyuiu.cources.programming.interfaces.dll'
+               
         self.__generate_hello_table()
         self.__generate_app_names()
         self.__generate_programs_code()
         
     def __generate_hello_table(self):
+        if (self.sprint_number == 6) :
+          self.console_table = ""
+          return
         condition = self.condition
         title = self.title
         task = self.task_number
@@ -103,13 +108,14 @@ class Program:
         self.console_app_name = f"Tyuiu.{fio}.Sprint{sprint}.Task{task}.V{variant}"
         self.library_name = f"Tyuiu.{fio}.Sprint{sprint}.Task{task}.V{variant}.Lib"
         self.mstest_name = f"Tyuiu.{fio}.Sprint{sprint}.Task{task}.V{variant}.Test"
-    
+ 
     def __generate_programs_code(self):
         sprint = self.sprint_number
         task = self.task_number
         variant = self.variant_number
         fio = self.initials
-        self.console_app_code = """using Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+""".Lib;
+        if (self.sprint_number != 6): # Generate general console app code
+            self.console_app_code = """using Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+""".Lib;
 namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+"""
 {
     class Program
@@ -126,7 +132,92 @@ namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""
         }
     }
 }"""
-        
+        else: # GENERATE CODE FOR WINFORMS APP IN 6 SPRINT
+            self.console_app_code = ""
+            self.form_main_code = """using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+""".Lib;
+
+namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+"""
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+    }
+}"""
+            self.form_designer_code = """namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+""";
+            
+partial class FormMain
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Text = "Form1";
+    }
+
+    #endregion
+}"""        
+
+            self.program_code = """using Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+""".Lib;
+namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+"""
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FormMain());
+        }
+    }
+}"""
+            
+            ### END 6 SPRINT CODE GENERATING ###
+            
+		# GENERATE LIBRARY CODE
         self.library_code = """using tyuiu.cources.programming.interfaces.Sprint"""+str(sprint)+""";
 
 namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+""".Lib
@@ -137,7 +228,7 @@ namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""
     }
 }
 """
-
+		# GENERATE MSTEST CODE
         self.mstest_code = """namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""+str(variant)+""".Test
 {
     public class DataServiceTest
@@ -160,6 +251,11 @@ namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""
                 print(f"Solution {self.solution_folder_name} был успешно создан!")
             else:
                 print("Ошибка при создании solution.")
+                
+    def __copy_dll(self):
+        path = f"{self.path[:-1] + str(self.sprint_number-1)}\\{self.shit_dll}"
+        if os.path.exists(path):
+            os.system(f'copy "{path}" "{os.path.join(self.path, self.shit_dll)}"')
     
     def __create_projects(self):
         console_app_exists = os.path.exists(self.path+'\\'+self.console_app_name)
@@ -192,6 +288,22 @@ namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""
                     },
                 }
         )
+        if self.sprint_number == 6:
+            projects_dicts['console_app'] = {
+				'terminal': 'winforms',
+				'folder_name': self.console_app_name,
+				'file_to_edit': ('Form1.cs', 'Form1.Designer.cs', 'Program.cs'),
+				'file_code': {
+                    'Form1.cs': self.form_main_code,
+                    'Form1.Designer.cs': self.form_designer_code, 
+                    'Program.cs': self.program_code
+                    },
+				'new_file_name': {
+                    'Form1.cs': 'FormMain.cs',
+                    'Form1.Designer.cs': 'FormMain.Designer.cs'
+                    },
+				'exists': console_app_exists,
+			}
         # create and modify projects: console_app, library, mstest
         for project_name, project_dict in projects_dicts.items():
             terminal = project_dict['terminal']
@@ -214,14 +326,30 @@ namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""
             
                 # rewrite the code    
                 try:
-                    # get .cs file in project directory to modify and rename it
-                    file_to_edit_list = [file for file in os.listdir(folder_name) if file.split('.')[-1] == 'cs']
-                    file_to_edit = file_to_edit_list[0] if len(file_to_edit_list) >= 1 else None
-                    if new_file_name is not None and file_to_edit is not None:
-                        os.rename(folder_name+'\\'+file_to_edit, folder_name+'\\'+new_file_name)
-                        file_to_edit = new_file_name
-                    with open(folder_name+'\\'+file_to_edit, 'w', encoding='utf-8-sig') as file:
-                        file.write(file_code)   
+                    # for all sprint except 6 do the old code to not break the program
+                    if (self.sprint_number != 6 or project_name!='console_app'):
+                        # get .cs file in project directory to modify and rename it
+                        file_to_edit_list = [file for file in os.listdir(folder_name) if file.split('.')[-1] == 'cs']
+                        file_to_edit = file_to_edit_list[0] if len(file_to_edit_list) >= 1 else None
+                        if new_file_name is not None and file_to_edit is not None:
+                            os.rename(folder_name+'\\'+file_to_edit, folder_name+'\\'+new_file_name)
+                            file_to_edit = new_file_name
+                        with open(folder_name+'\\'+file_to_edit, 'w', encoding='utf-8-sig') as file:
+                            file.write(file_code)
+                    # new code for 6 sprint
+                    else:
+                        for file in file_to_edit:
+                            # rewrite the code
+                            if file_code is not None and file is not None:
+                                if file_code.get(file, None) is not None:
+                                    with open(folder_name+'\\'+file, 'w', encoding='utf-8-sig') as f:
+                                        f.write(file_code[file])
+                            # rename file
+                            if new_file_name is not None and file is not None:
+                                if new_file_name.get(file, None) is not None:  
+                                    os.rename(folder_name+'\\'+file, folder_name+'\\'+new_file_name[file])
+                    print("Файлы успешно отредактированы.")
+                        
                 except:
                     print("Ошибка при редактиваровании файлов проекта.")
             else:
@@ -252,7 +380,7 @@ namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""
                     item_group = ET.Element('ItemGroup')
                     reference = ET.SubElement(item_group, 'Reference', Include="tyuiu.cources.programming.interfaces")
                     hint_path = ET.SubElement(reference, 'HintPath')
-                    hint_path.text = '..\\tyuiu.cources.programming.interfaces.dll'
+                    hint_path.text = f'..\\{self.shit_dll}'
                     
                     for element in root:
                         if element.tag == 'PropertyGroup':
@@ -266,6 +394,7 @@ namespace Tyuiu."""+fio+""".Sprint"""+str(sprint)+""".Task"""+str(task)+""".V"""
 
     def create(self):
         self.__create_solution()
+        self.__copy_dll()
         os.chdir(self.path)
         self.__create_projects()
         
